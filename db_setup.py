3835e07da16ea93b1812150f6a0facb9ca44e2ef
TingDeng <tiadeng@gmail.com>
2017-07-15 15:20:43 -0400

    Update db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -21,6 +21,7 @@ class City(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
+    trip = relationship('Trip',cascade="save-update,merge,delete")
     user = relationship(User)
 
     @property
